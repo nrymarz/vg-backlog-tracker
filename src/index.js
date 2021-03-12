57d8312b69d266cc4f715707ab4976262ed6ccf7
Nathan Rymarz <nrymarz@gmail.com>
2021-03-11 18:36:41 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux'
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import videoGameList from './reducers/videogamelist'
+
+const store=createStore(videoGameList,applyMiddleware(thunk))
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ src/reducers/videogamelist.js
@@ -0,0 +1,8 @@
+const videoGameList = (state={},action) =>{
+    switch(action.type){
+        default:
+            return state
+    }
+}
+
+export default videoGameList
