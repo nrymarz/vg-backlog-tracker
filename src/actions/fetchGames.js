b2ce23e52f3683703ba826d8e92bca03b6043726
Nathan Rymarz <nrymarz@gmail.com>
2021-03-12 15:51:59 -0600

    got .env working with dotenv

--- src/actions/fetchGames.js
+++ src/actions/fetchGames.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
 const url = "https://api.rawg.io/api/games?"
+const api_key = `key=${process.env.REACT_APP_API_KEY}`
 
 const fetchGames = (params) => {
+    console.log(process.env)
     return dispatch =>{
         dispatch({type: "START_ADDING_GAMES"})
         fetch(url + api_key)
