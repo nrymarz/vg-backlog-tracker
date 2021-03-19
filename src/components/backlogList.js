a632c6d749ba83533d091477f88f6b2f125d5cfd
Nathan Rymarz <nrymarz@gmail.com>
2021-03-19 11:50:23 -0500

    added function for removing games

--- src/components/backlogList.js
+++ src/components/backlogList.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ListGroup from 'react-bootstrap/ListGroup'
 import Dropdown from 'react-bootstrap/Dropdown'
 
-export default function BacklogList({games, variant, handleSelect}){
+export default function BacklogList({games, variant, handleSelect, handleRemove}){
 
     function renderGames(){
         return games.map( game=> {
@@ -16,7 +16,7 @@ export default function BacklogList({games, variant, handleSelect}){
                                 <Dropdown.Item disabled={game.status==="NOT_STARTED"} eventKey='NOT_STARTED' onSelect={event => handleSelect(event,game)}>Move To Backlog</Dropdown.Item>
                                 <Dropdown.Item disabled={game.status==="STARTED"} eventKey='STARTED' onSelect={event => handleSelect(event,game)}>Move To Currently Playing</Dropdown.Item>
                                 <Dropdown.Item disabled={game.status==="COMPLETED"} eventKey='COMPLETED' onSelect={event => handleSelect(event,game)}>Move To Completed</Dropdown.Item>
-                                <Dropdown.Item eventKey='REMOVE' onSelect={event => handleSelect(event,game)}>Remove Game</Dropdown.Item>
+                                <Dropdown.Item onSelect={() => handleRemove(game)}>Remove Game</Dropdown.Item>
                             </Dropdown.Menu>
                         </Dropdown>
                     </h5>
--- src/containers/backlog.js
+++ src/containers/backlog.js
@@ -23,22 +23,31 @@ class Backlog extends Component {
         })
     }
 
+    handleRemove = game =>{
+        this.props.removeGame(game)
+        this.setState( prevState =>{
+            return{
+                [game.status]: prevState[game.status].filter(g => g.id !== game.id)
+            } 
+        })
+    }
+
     render(){
         return(
             <Row className="text-center">
                  <Col xs={4} >
                     <h2>Backlog:</h2>
-                    <BacklogList games={this.state.NOT_STARTED} variant="danger" handleSelect={this.handleSelect}/>
+                    <BacklogList games={this.state.NOT_STARTED} variant="danger" handleSelect={this.handleSelect} handleRemove={this.handleRemove}/>
                 </Col>
 
                 <Col xs={4}>
                     <h2>Currently Playing:</h2>
-                    <BacklogList games={this.state.STARTED} variant="info" handleSelect={this.handleSelect}/>
+                    <BacklogList games={this.state.STARTED} variant="info" handleSelect={this.handleSelect} handleRemove={this.handleRemove}/>
                 </Col>
                
                 <Col xs={4}>
                     <h2>Completed:</h2>
-                    <BacklogList games={this.state.COMPLETED} variant="success" handleSelect={this.handleSelect}/>
+                    <BacklogList games={this.state.COMPLETED} variant="success" handleSelect={this.handleSelect} handleRemove={this.handleRemove}/>
                 </Col>
             </Row>
         )
