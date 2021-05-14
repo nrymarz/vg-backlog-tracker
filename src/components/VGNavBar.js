9622cc3f9350d5f466b264cec0bf9a14b8f4317e
Nathan Rymarz <nrymarz@gmail.com>
2021-05-14 12:22:29 -0500

    added method for hiding and showing nav menu onclick of burger menu

--- src/components/VGNavBar.js
+++ src/components/VGNavBar.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import BurgerMenu from './burgerMenu'
 import {Link} from 'react-router-dom'
 
+
 export default function VGNavBar({user}){
+
+    const targetRef = useRef(null)
 
     function renderUser(){
         if(user.length > 0) return <Navbar.Text className="mx-3">Logged In as {user}</Navbar.Text>
@@ -14,11 +17,16 @@ export default function VGNavBar({user}){
         if(user.length > 0) return <Nav.Link as={Link} to='/logout'>Log Out</Nav.Link>
         return <Nav.Link as={Link} to='/login'>Log In</Nav.Link>
     }
+
+    function changeMenu(open){
+        if(open) targetRef.current.style.display="none"
+        else targetRef.current.style.display="flex"
+    }
     
     return(
         <Navbar variant='dark'>
             <Navbar.Brand>VG Backlog Tracker</Navbar.Brand>
-            <Nav defaultActiveKey="/">
+            <Nav defaultActiveKey="/" ref={targetRef}>
                 <Nav.Link as={Link} to="/">Search</Nav.Link>
                 <Nav.Link as={Link} to="/backlog">Backlog</Nav.Link>
                 {renderLogin()}
@@ -26,7 +34,7 @@ export default function VGNavBar({user}){
             <Nav className="ml-auto mr-3">
                 {renderUser()}
                 <Nav.Link href="https://rawg.io">Rawg.io</Nav.Link>
-                <BurgerMenu/>
+                <BurgerMenu changeMenu={changeMenu}/>
             </Nav>
         </Navbar>
     )
--- src/components/burgerMenu.js
+++ src/components/burgerMenu.js
@@ -1,10 +1,15 @@
 import React, {useState} from "react"
 
-export default function BurgerMenu(){
+export default function BurgerMenu({changeMenu, menu}){
     const [open, setOpen] = useState(false)
 
+    const handleClick = e =>{
+        setOpen(!open)
+        changeMenu(open)
+    }
+
     return(
-        <div className="burger-menu" onClick={() => setOpen(!open)}>
+        <div className="burger-menu" onClick={handleClick} style={{background: open ? "white" : "grey"}}>
             <div />
             <div />
             <div />
