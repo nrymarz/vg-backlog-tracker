483a82a5964eddff43366b928ed5de772ccad548
Nathan Rymarz <nrymarz@gmail.com>
2021-05-14 13:31:02 -0500

    edited phone-nav to show login info and rawg link and added hide-phone class to user/rawg nav

--- src/components/VGNavBar.js
+++ src/components/VGNavBar.js
@@ -32,17 +32,19 @@ export default function VGNavBar({user}){
                     <Nav.Link as={Link} to="/backlog">Backlog</Nav.Link>
                     {renderLogin()}
                 </Nav>
-                <Nav className="ml-auto mr-3">
+                <Nav className="ml-auto mr-3 hideable-phone">
                     {renderUser()}
                     <Nav.Link href="https://rawg.io">Rawg.io</Nav.Link>
-                    <BurgerMenu changeMenu={changeMenu}/>
                 </Nav>
+                <BurgerMenu changeMenu={changeMenu}/>
             </Navbar>
             <Navbar variant='dark' className="phone-nav" ref={targetRef}>
                 <Nav defaultActiveKey="/" className="d-flex flex-column">
                     <Nav.Link as={Link} to="/">Search</Nav.Link>
                     <Nav.Link as={Link} to="/backlog">Backlog</Nav.Link>
+                    {renderUser()}
                     {renderLogin()}
+                    <Nav.Link href="https://rawg.io">Rawg.io</Nav.Link>
                 </Nav>
             </Navbar>
         </>
